e4699caca5c953622ca5826b6c83d2c052b39dbd
Dmitry Mironov <superparabolic@gmail.com>
2014-11-11 20:58:06 +0300

    minor changes

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -1,5 +1,9 @@
 <?php
 
+use Eluceo\iCal\Component\Calendar;
+use Eluceo\iCal\Component\Event;
+use Eluceo\iCal\Property\Event\RecurrenceRule;
+
 class APIController extends Controller {
 
 	public function getGroups()
@@ -27,6 +31,33 @@ class APIController extends Controller {
 			return $schedule;
 		}
 		App::abort(404);
+	}
+
+	public function getSchedule($id) {
+		$pairs = Schedule::find($id)->pairs;
+		$cal = new Calendar('mguesi.ru');
+		// setup data
+		$now = new \DateTime();
+		$pairs->each(function(Pair $pair) use (&$cal) {
+			$event = new Event();
+			$event->setDtStart(new \DateTime('2014-11-11 13:00:00')); // @TODO: change datetime
+			$event->setDtEnd(new \DateTime('2014-11-11 13:30:00')); // @TODO: change datetime
+			$event->setSummary($pair->name); // @TODO: add type
+			$event->setDescription($pair->teacher);
+			$event->setLocation($pair->location);
+			$recurrenceRule = new RecurrenceRule();
+			$recurrenceRule->setFreq(RecurrenceRule::FREQ_WEEKLY);
+			$recurrenceRule->setInterval(2);
+			$recurrenceRule->setCount(10);
+			$event->setRecurrenceRule($recurrenceRule);
+			$cal->addComponent($event);
+		});
+		$response = Response::make($cal->render(), 200, [
+			'Content-Type' => 'text/calendar; charset=utf-8',
+			'Content-Disposition' => 'attachment; filename="cal.ics"'
+		]);
+
+		return $response;
 	}
 
 }
