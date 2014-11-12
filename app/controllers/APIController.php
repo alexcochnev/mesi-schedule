6c0eb9c545697bcc9430e7f6ff7451f9f5376cb1
Dmitry Mironov <superparabolic@gmail.com>
2014-11-12 23:46:01 +0300

    setting date in the generating calendar

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Eluceo\iCal\Component\Calendar;
 use Eluceo\iCal\Component\Event;
 use Eluceo\iCal\Property\Event\RecurrenceRule;
@@ -34,22 +35,38 @@ class APIController extends Controller {
 	}
 
 	public function getSchedule($id) {
+
 		$pairs = Schedule::find($id)->pairs;
 		$cal = new Calendar('mguesi.ru');
-		// setup data
-		$now = new \DateTime();
-		$pairs->each(function(Pair $pair) use (&$cal) {
+
+        $ss = $this->getSemesterStart(new Carbon());
+		$pairs->each(function(Pair $pair) use (&$cal, $ss) {
 			$event = new Event();
-			$event->setDtStart(new \DateTime('2014-11-11 13:00:00')); // @TODO: change datetime
-			$event->setDtEnd(new \DateTime('2014-11-11 13:30:00')); // @TODO: change datetime
-			$event->setSummary($pair->name); // @TODO: add type
+
+            $day = $ss->copy();
+            $day->day += $pair->day + ($pair->week === 'even' ? 0: 7);
+            $pair_start = $day->copy();
+            $pair_start->addMinutes($pair->getStartTime());
+            $pair_end = $pair_start->copy();
+            $pair_end->addMinutes(90);
+
+            // init event
+			$event->setDtStart($pair_start);
+			$event->setDtEnd($pair_end);
+			$event->setSummary($pair->type . ' ' . $pair->name); // @TODO: short type
 			$event->setDescription($pair->teacher);
 			$event->setLocation($pair->location);
+
+            // add recurrence rule
 			$recurrenceRule = new RecurrenceRule();
 			$recurrenceRule->setFreq(RecurrenceRule::FREQ_WEEKLY);
 			$recurrenceRule->setInterval(2);
-			$recurrenceRule->setCount(10);
+			$recurrenceRule->setCount(10); // @FIXME
 			$event->setRecurrenceRule($recurrenceRule);
+
+            $event->setUseTimezone(true);
+
+            // add event to calendar
 			$cal->addComponent($event);
 		});
 		$response = Response::make($cal->render(), 200, [
@@ -59,5 +76,25 @@ class APIController extends Controller {
 
 		return $response;
 	}
+
+    /**
+     * @param Carbon $date
+     * @return Carbon
+     */
+    protected function getSemesterStart(Carbon $date)
+    {
+        $semester_start = Carbon::now();
+
+        if ($date->month >= 9) {
+            $semester_start->setDate($date->year, 9, 1);
+        } else if ($date->month < 2) {
+            $semester_start->setDate($date->year-1, 9, 1);
+        } else {
+            $semester_start->setDate($date->year, 2, 1);
+        }
+        $semester_start->setTime(0, 0, 0);
+
+        return $semester_start;
+    }
 
 }
