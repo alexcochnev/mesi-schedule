af5581fe083017361c6007d636350315bb6b2fb3
Dmitry Mironov <superparabolic@gmail.com>
2014-11-13 19:52:05 +0300

    spaces to tabs

--- app/controllers/APIController.php
+++ app/controllers/APIController.php
@@ -39,34 +39,34 @@ class APIController extends Controller {
 		$pairs = Schedule::find($id)->pairs;
 		$cal = new Calendar('mguesi.ru');
 
-        $ss = $this->getSemesterStart(new Carbon());
+		$ss = $this->getSemesterStart(new Carbon());
 		$pairs->each(function(Pair $pair) use (&$cal, $ss) {
 			$event = new Event();
 
-            $day = $ss->copy();
-            $day->day += $pair->day + ($pair->week === 'even' ? 0: 7);
-            $pair_start = $day->copy();
-            $pair_start->addMinutes($pair->getStartTime());
-            $pair_end = $pair_start->copy();
-            $pair_end->addMinutes(90);
+			$day = $ss->copy();
+			$day->day += $pair->day + ($pair->week === 'even' ? 0: 7);
+			$pair_start = $day->copy();
+			$pair_start->addMinutes($pair->getStartTime());
+			$pair_end = $pair_start->copy();
+			$pair_end->addMinutes(90);
 
-            // init event
+			// init event
 			$event->setDtStart($pair_start);
 			$event->setDtEnd($pair_end);
 			$event->setSummary($pair->type . ' ' . $pair->name); // @TODO: short type
 			$event->setDescription($pair->teacher);
 			$event->setLocation($pair->location);
 
-            // add recurrence rule
+			// add recurrence rule
 			$recurrenceRule = new RecurrenceRule();
 			$recurrenceRule->setFreq(RecurrenceRule::FREQ_WEEKLY);
 			$recurrenceRule->setInterval(2);
 			$recurrenceRule->setCount(10); // @FIXME
 			$event->setRecurrenceRule($recurrenceRule);
 
-            $event->setUseTimezone(true);
+			$event->setUseTimezone(true);
 
-            // add event to calendar
+			// add event to calendar
 			$cal->addComponent($event);
 		});
 		$response = Response::make($cal->render(), 200, [
@@ -77,24 +77,24 @@ class APIController extends Controller {
 		return $response;
 	}
 
-    /**
-     * @param Carbon $date
-     * @return Carbon
-     */
-    protected function getSemesterStart(Carbon $date)
-    {
-        $semester_start = Carbon::now();
+	/**
+	 * @param Carbon $date
+	 * @return Carbon
+	 */
+	protected function getSemesterStart(Carbon $date)
+	{
+		$semester_start = Carbon::now();
 
-        if ($date->month >= 9) {
-            $semester_start->setDate($date->year, 9, 1);
-        } else if ($date->month < 2) {
-            $semester_start->setDate($date->year-1, 9, 1);
-        } else {
-            $semester_start->setDate($date->year, 2, 1);
-        }
-        $semester_start->setTime(0, 0, 0);
+		if ($date->month >= 9) {
+			$semester_start->setDate($date->year, 9, 1);
+		} else if ($date->month < 2) {
+			$semester_start->setDate($date->year-1, 9, 1);
+		} else {
+			$semester_start->setDate($date->year, 2, 1);
+		}
+		$semester_start->setTime(0, 0, 0);
 
-        return $semester_start;
-    }
+		return $semester_start;
+	}
 
 }
--- app/models/Pair.php
+++ app/models/Pair.php
@@ -2,8 +2,8 @@
 
 class Pair extends Eloquent {
 
-    const PAIR_LENGTH = 90;
-    const BREAK_LENGTH = 10;
+	const PAIR_LENGTH = 90;
+	const BREAK_LENGTH = 10;
 
 	protected $table = 'pair';
 
@@ -16,21 +16,21 @@ class Pair extends Eloquent {
 		return $this->belongsTo('Schedule');
 	}
 
-    /**
-     * @param integer $num
-     * @return integer minutes
-     */
-    public function getStartTime ()
-    {
-        return 510 + ($this->num-1)*self::PAIR_LENGTH + ($this->num-1)*self::BREAK_LENGTH + ($this->num < 4 ? 0 : 20);
-    }
+	/**
+	 * @param integer $num
+	 * @return integer minutes
+	 */
+	public function getStartTime ()
+	{
+		return 510 + ($this->num-1)*self::PAIR_LENGTH + ($this->num-1)*self::BREAK_LENGTH + ($this->num < 4 ? 0 : 20);
+	}
 
-    /**
-     * @param $num
-     * @return integer $minutes
-     */
-    public function getFinishTime ()
-    {
-        return 510 + $this->num*self::PAIR_LENGTH + + ($this->num-1)*self::BREAK_LENGTH + ($this->num < 4 ? 0 : 20);
-    }
+	/**
+	 * @param $num
+	 * @return integer $minutes
+	 */
+	public function getFinishTime ()
+	{
+		return 510 + $this->num*self::PAIR_LENGTH + + ($this->num-1)*self::BREAK_LENGTH + ($this->num < 4 ? 0 : 20);
+	}
 }
