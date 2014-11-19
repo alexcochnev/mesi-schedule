3df9d92ba594bf93ce9db23a9fa0cd88a5e2f7a9
Dmitry Mironov <superparabolic@gmail.com>
2014-11-20 01:18:52 +0300

    use truncate instead delete to reset indexes

--- app/database/seeds/GroupTableSeeder.php
+++ app/database/seeds/GroupTableSeeder.php
@@ -4,7 +4,7 @@ class GroupTableSeeder extends DatabaseSeeder {
 
 	public function run()
 	{
-		DB::table('group')->delete();
+		DB::table('group')->truncate();
 
 		Group::create(array('mesi_id' => 1, 'name' => 'ДКП-123б'));
 		Group::create(array('mesi_id' => 2, 'name' => 'ДКП-123бс'));
